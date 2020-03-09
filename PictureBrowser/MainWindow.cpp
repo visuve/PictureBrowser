fb1a3f992ffe004fe0b05ba5ecab003b9fa622e3
visuve <veli-matti.visuri@iki.fi>
2020-03-09 21:43:41 +0200

    Add scroll bar to file list
    - https://docs.microsoft.com/en-us/windows/win32/controls/list-box-styles

--- PictureBrowser/MainWindow.cpp
+++ PictureBrowser/MainWindow.cpp
@@ -336,7 +336,7 @@ namespace PictureBrowser
 		m_fileListBox = CreateWindow(
 			WC_LISTBOX,
 			L"Filelist...",
-			WS_VISIBLE | WS_CHILD | LBS_NOTIFY | LBS_SORT,
+			WS_VISIBLE | WS_CHILD | LBS_STANDARD,
 			0,
 			0,
 			FileListWidth,
